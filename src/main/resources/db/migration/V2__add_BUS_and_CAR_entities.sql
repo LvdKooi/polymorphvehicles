5edc4770d271528af37084e22996b01895efebab
Laurens van der Kooi <laurens.van.der.kooi@ordina.nl>
2023-07-26 10:59:17 +0200

    refactoring

--- src/main/resources/db/migration/V2__add_BUS_and_CAR_entities.sql
+++ src/main/resources/db/migration/V2__add_BUS_and_CAR_entities.sql
@@ -1,8 +1,8 @@
-alter table VEHICLE_ENTITY
+alter table vehicle_entity
     add column BUS_TYPE VARCHAR(10);
-alter table VEHICLE_ENTITY
+alter table vehicle_entity
     add column LITERS_LUGGAGE_CAPACITY BIGINT;
-alter table VEHICLE_ENTITY
+alter table vehicle_entity
     add column BODY_STYLE VARCHAR(20);
-alter table VEHICLE_ENTITY
+alter table vehicle_entity
     add column NUMBER_OF_DOORS BIGINT;
